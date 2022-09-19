17000c773aed61d4dd61cae02afe49d185d5411d
EmadKazi-ux <111903215+EmadKazi-ux@users.noreply.github.com>
2022-09-19 11:31:27 -0400

    commit

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+public class Main {
+    public Main() {
+
+    }
+
+    public static void main(String[] args) {
+
+        double price = 50;
+
+        double tax = price * 0.05;
+
+        double total = price + tax;
+
+        System.out.println("total tax on your price will be " + tax + " and total price of item will be " + total);
+
+
+
+
+
+
+
+    }
+}
